1cc51e3541a82b46758d6c3befd2e4d9d8527d36
Artur Freires <savioria247@gmail.com>
2023-08-06 22:58:20 -0700

    Add files via upload

---
+++ pycode/Variables.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 22
+
+print(x)
+
+
+# In[2]:
+
+
+type(x)
+
+
+# In[6]:
+
+
+y = 'I am a String'
+print(y)
+
+
+# In[7]:
+
+
+y = 'I am a different string'
+print(y)
+
+
+# In[9]:
+
+
+x,y,z = 'Chocolate', 'Vanilla', 'Rocky Road'
+print(x)
+print(y)
+print(z)
+
+
+# In[11]:
+
+
+x = y = z = 'All the things'
+print(x)
+print(y)
+print(z)
+
+
+# In[14]:
+
+
+ice_cream = ['Chocolate', 'Vanilla', 'Rocky Road']
+x,y,z = ice_cream
+
+print(x)
+print(y)
+print(z)
+
+
+# In[15]:
+
+
+#Camel case
+
+#Test Variable Case
+
+testVariableCase = 'Vanilla'
+
+
+# In[16]:
+
+
+#Pascal case
+
+#Test Variable Case
+
+TestVariableCase = 'Vanilla'
+
+
+# In[ ]:
+
+
+#Snake case
+
+#Test Variable Case
+
+test_variable_case = 'Vanilla'
+
+
+# In[19]:
+
+
+#Some examples
+
+testvar = 'Vanilla'
+test_var = 'Vanilla'
+_test_var = 'Vanilla'
+testVar = 'Vanilla'
+TestVar = 'Vanilla'
+testVar2 = 'Vanilla'
+
+
+# In[22]:
+
+
+#Concatenating
+
+x = 'I can add a period, like this: ' + '.'
+print(x)
+
+
+# In[23]:
+
+
+y = 3 + 2
+print(y)
+
+
+# In[24]:
+
+
+x = 'Ice Cream'
+y = ' is'
+z = ' my favorite'
+print(x+y+z)
+
+
+# In[25]:
+
+
+x = 2
+y = 3
+z = 3
+print(x+y+z)
+
+
+# In[27]:
+
+
+x = 'Ice Cream'
+y = 2
+
+print(x,y)
+
+
+# In[ ]:
+
+
+
+
