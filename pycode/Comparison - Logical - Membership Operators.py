bf44c7785984c945a5c5a0d59a3cf69468ecff38
Artur Freires <savioria247@gmail.com>
2023-08-07 01:23:57 -0700

    Add files via upload

---
+++ pycode/Comparison - Logical - Membership Operators.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+10 == 50
+
+
+# In[2]:
+
+
+10 != 50
+
+
+# In[3]:
+
+
+'Vanilla' == 'Vanilla'
+
+
+# In[5]:
+
+
+x = 'Chocolate'
+y = 'Vanilla'
+
+x != y
+
+
+# In[6]:
+
+
+10 <= 10
+
+
+# In[7]:
+
+
+10 >= 10
+
+
+# In[8]:
+
+
+(10 > 50) and (50 < 10)
+
+
+# In[9]:
+
+
+(70 > 50) and (50 > 10)
+
+
+# In[10]:
+
+
+(70 > 50) or (50 < 10)
+
+
+# In[11]:
+
+
+#Strings compared will check the alphabet order
+('Vanilla' > 'Chocolate') and (50 > 10)
+
+
+# In[12]:
+
+
+ice_cream = 'I love Chocolate ice cream'
+'love' in ice_cream
+
+
+# In[15]:
+
+
+scoops = [1,2,3,4,5]
+wanted_scoops = 6
+6 not in scoops
+
+
+# In[ ]:
+
+
+
+
