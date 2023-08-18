81e7cbc76f69bfe43dfbe4dc02583ed82b3ce51b
Artur Freires <savioria247@gmail.com>
2023-08-17 23:40:21 -0700

    Add files via upload

---
+++ pycode/API Test Notebook.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+
+
+# In[4]:
+
+
+type(data)
+
+
+# In[8]:
+
+
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+
+
+# In[9]:
+
+
+pd.json_normalize(data['data'])
+
+
+# In[ ]:
+
+
+
+
